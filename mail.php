75f4021c691010a9b6b31b581c346844a777c36a
karthik2628 <karthikkishore2603@gmail.com>
2023-05-28 09:17:15 +0530

    mail

--- mail.php
+++ mail.php
@@ -3,10 +3,10 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
+$to = "karthikkishore2603@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
+$headers = "From: support@karthikkishore";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
